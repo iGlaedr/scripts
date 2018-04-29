92bb3b97a304b2287696f37276009e3ef40b0430
iGlaedr <jannik.tom@gmail.com>
2018-04-29 19:20:08 +0200

    Update slavev4.sql
    Automatic IDs

--- pdns/slavev4.sql
+++ pdns/slavev4.sql
@@ -1,2 +1,2 @@
 use powerdns;
-INSERT INTO domains VALUES ('example.com','placeNS0:53',1524317412,'SLAVE',NULL,'');
+INSERT INTO domains (name, master, type, notified_serial,account) VALUES ('example.com','placeNS0:53','SLAVE',NULL,'');
